36ccf33c726f28a066c6e60835df7afd23bb52fd
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2024-08-05 14:32:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def generate_certificate(name, start_date, end_date, issue_date):
     width, height = letter
 
     # Add logos at the top center
-    c.drawImage('image.png', 250, height - 100, 100, 50)  # Adjust the path and size as necessary
+    c.drawImage('image.png', 250, height - 120, 120, 60)  # Adjust the path and size as necessary
 
     # Add border
     c.setStrokeColor(colors.navy)
